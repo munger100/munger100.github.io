37c020befe8344155e79327aedc39024225b3f9e
Matthew Munger <mattfromthecountry@hotmail.com>
2014-04-17 14:43:54 -0400

    Initial Commit

---
+++ PracticeJQ.js
@@ -0,0 +1,15 @@
+/**
+ * Created by matthew on 4/14/2014.
+ */
+button = document.getElementById("button");
+
+$( document ).ready(function() {
+    button = document.getElementById("button");
+    $("button").click(click);
+});
+
+
+function click()
+{
+    alert(hi);
+}
---
+++ PracticeJS.js
@@ -0,0 +1,16 @@
+// Created by matthew on 4/14/2014
+
+window.onload = function () {
+
+    var x = 1;
+    var y = 2;
+    var z = x + y
+    var input = prompt("hello", "sup?");
+
+
+    button = document.getElementById("button");
+    button.addEventListener("click", clicked);
+    function clicked() {
+        alert(input);
+    }
+}
