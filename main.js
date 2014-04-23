7508169209ab3c30472600de3876cf6109f9118d
Matthew Munger <mattfromthecountry@hotmail.com>
2014-04-23 17:55:15 -0400

    Tried to add Mckernan28

--- main.js
+++ main.js
@@ -6,17 +6,24 @@ var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
+var yellow = [255, 255, 0];
 
-var myName = "Munger100";
-var letterColors = [red, orange, green];
-if (15 == 15)
-{
-    bubbleShape = "circle";
-}
-else
-{
-    bubbleShape = "square"
-}
+var myName = "Dalaw1";
+var letterColors = [purple, orange, yellow];
+bubbleShape = "circle";
+var otherName = "Mckernan28";
+var otherLetterColors = [blue, green, red];
+var otherBubbleShape = "square";
+
+
+
+
+
+
+
 
 drawName(myName, letterColors);
+bounceBubbles();
+
+drawName(otherName, otherLetterColors);
 bounceBubbles();
