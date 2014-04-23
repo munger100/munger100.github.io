025765a07f9ec180f0728b6639d087574ef8e678
Matthew Munger <mattfromthecountry@hotmail.com>
2014-04-23 17:41:40 -0400

    Added awesome stuff

---
+++ main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by matthew on 4/23/2014.
+ */
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Munger100";
+var letterColors = [red, orange, green];
+if (15 == 15)
+{
+    bubbleShape = "circle";
+}
+else
+{
+    bubbleShape = "square"
+}
+
+drawName(myName, letterColors);
+bounceBubbles();
