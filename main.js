c29b6b74e10da3a94100b1f6b3c6bbc790b3c66a
Matthew Munger <mattfromthecountry@hotmail.com>
2014-04-23 19:44:17 -0400

    Fun

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 /**
  * Created by matthew on 4/23/2014.
  */
-var red = [0, 100, 63];
-var orange = [40, 100, 60];
-var green = [75, 100, 40];
-var blue = [196, 77, 55];
-var purple = [280, 50, 60];
-var yellow = [255, 255, 0];
+window.onload = function () {
 
-var myName = "Meggs is smelly";
-var letterColors = [red, orange, blue];
-bubbleShape = "circle";
+    var red = [0, 100, 63];
+    var orange = [40, 100, 60];
+    var green = [75, 100, 40];
+    var blue = [196, 77, 55];
+    var purple = [280, 50, 60];
+    var yellow = [255, 255, 0];
+    var input = document.getElementById('input');
+    var canvas = document.getElementById('myCanvas');
+    var myName = "Meggs is smelly";
+    var letterColors = [red, orange, blue];
 
+    input.addEventListener("change", function ()
+    {
+        myName = input.innerHTML;
+    });
 
+    bubbleShape = "circle";
 
+    drawName(myName, letterColors);
+    bounceBubbles();
+};
 
-
-
-
-
-
-drawName(myName, letterColors);
-bounceBubbles();
