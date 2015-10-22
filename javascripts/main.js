54a6ca0cb3becaf81f9eff41c8b17e78d16fda33
Matthew Munger <matthew.munger@outlook.com>
2015-10-22 17:27:06 -0400

    Tried to make login open in same tab

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+// Login
+function check(form)/*function to check userid & password*/
+{
+    /*the following code checks whether the entered userid and password are matching*/
+    if(form.userid.value == "munger100" && form.pwd.value == "Matthew11")
+    {
+        window.open("./edit.html", "_top")/*opens the target page while Id & password matches*/
+    }
+    else
+    {
+        alert("Error Password or Username")/*displays error message*/
+    }
+}
