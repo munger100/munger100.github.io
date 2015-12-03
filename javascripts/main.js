2a8dff0a4ebd4302a95445b000e40f308f555b86
Matthew Munger <matthew.munger@outlook.com>
2015-12-03 15:50:31 -0400

    Made list work with js script, only have problems 1 to 10 on the site for now.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,24 +1,29 @@
 // Add all the drop down menus for all the menus
 
 $(document).ready(function () {
-    $("#list").html("<p>nothing</p>");
-    // Drop Down Menu
-    $(function () {
-        $('li > ul').each(function (i) {
-            var parent_li = $(this).parent('li');
-            parent_li.addClass('folder');
-            var sub_ul = $(this).remove();
-            parent_li.wrapInner('<a/>').find('a').click(function () {
-                sub_ul.toggle();
-            });
-            parent_li.append(sub_ul);
-        });
-        $('ul ul').hide();
+    var completed = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var list = document.getElementById("list");
+    var list_innerHTML = null;
+    completed.forEach(function (num) {
+        list_innerHTML = list.innerHTML;
+        list.innerHTML = list_innerHTML +
+            '<li id="' + num + '">' +
+            '<h4 class="number">Problem #' + num + '</h4>' +
+            '<div class="hider">' +
+            '<h4 class="problemdesc"><a href="http://projecteuler.net/problem=' + num + '" target="_blank">Problem Description</a> </h4>' +
+            '<h4 class=problemscript"><a href="https://github.com/munger100/ProjectEulerPython/blob/master/' + num + '.py" target="_blank">Problem Script</a></h4>' +
+            '</div>' +
+            '</li>';
     });
-    /*
-     $.get("data.json", function(data) {
-     jsondata = JSON.parse(data);
-     console.log(jsondata);
-     });
-     */
+
+
+    // Drop Down Menu
+    $('li').each(function (i) {
+        var sub_ul = $(this).find("div.hider");
+        $(this).click(function () {
+            sub_ul.toggle();
+        });
+        sub_ul.hide();
+    });
+
 });
